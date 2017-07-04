32be7f950b3aea7db5d024b8b8c9f5a063277bf2
chuk-chuk <yulia.v.litvin@gmail.com>
2017-07-04 16:30:08 +0100

    add a test that expects DockingStation instances to respond_to the method release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,16 @@
 require 'docking_station'
 
 describe DockingStation do
+  # it "respond to release bike" do
+  #   docking_station = DockingStation.new
+  #   expect(docking_station).to respond_to(:release_bike)
+  # end
+
+  it { binding.pry; is_expected.to respond_to(:release_bike) }
 end
+
+# things to research!
+# described_class
+# subject
+# why does it short syntax work without declaring a subject?
+# research implict and explicit subject
