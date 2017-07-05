500af4ac96a843651111a38a2cecb703271145db
theartofnowt <michaelgordon.brown1@gmail.com>
2017-07-05 16:29:17 +0100

    step 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -14,6 +14,19 @@ describe DockingStation do
     expect(bike.working?).to eq true
   end
 
+  it "Docks stores an instance of a bike object when one is passed to it" do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    expect(docking_station.dock(bike)).to eq bike
+  end
+
+  it "shows if a bike is docked at the station" do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    docking_station.dock(bike)
+    expect(docking_station.bike).to be_a Bike
+  end
+
 
 end
 
