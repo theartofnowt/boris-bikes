d612aed423eb6f58836d5096d7d21f5c7a851d21
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-06 14:57:29 +0100

    Challenge 15 done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,19 +1,23 @@
 class DockingStation
   attr_reader 'bikes'
-
   def initialize
     @bikes =[]
     @capacity = 20
   end
-
   def release_bike
-    raise "Go away. No bike for you." if @bikes.empty?
+    raise "Go away. No bike for you." if empty?
     @bikes.pop
-
+  end
+  def dock(bike)
+    raise "No room for your bike. Go away." if full?
+    @bikes << bike
   end
 
-  def dock(bike)
-    raise "No room for your bike. Go away." if @bikes.length == @capacity
-    @bikes << bike
+  private
+  def full?
+    @bikes.count >= @capacity ? true : false
+  end
+  def empty?
+    @bikes.empty? ? true : false
   end
 end
